d500aa1985224d0881918b852367220619c417d4
mohamed gamal <76060169+gamy2@users.noreply.github.com>
2021-05-19 14:41:39 +0200

    logout handeld
    logout handled by mohamed gamal

--- lib/views/pages/HomePage.dart
+++ lib/views/pages/HomePage.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:iti_community_flutter/services/auth/Authentication.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,10 +12,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final loginProvider = Provider.of<AuthServices>(context);
     return Scaffold(
         body: SafeArea(
       child: Center(
-        child: Text("home"),
+        child: InkWell(
+          child: Text("Logout!"),
+          onTap: () {
+            loginProvider.logout();
+          },
+        ),
       ),
     ));
   }
